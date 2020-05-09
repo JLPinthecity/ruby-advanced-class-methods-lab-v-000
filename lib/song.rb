0f69f435bafded5b8b0f10af9a49b374fede23b7
Joann Pan <joannlpan@gmail.com>
2020-05-09 21:22:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,9 +18,9 @@ class Song
 
   def self.new_by_name(name)
     song = self.new
+    binding.pry
     song.name = song_name
     song
-    binding.pry
 #returns song instance with that name set as its name property
   end
 
