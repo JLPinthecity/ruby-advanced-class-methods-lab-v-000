b55a1b18f6d118cdd4d71886ac539f0bf701131d
Joann Pan <joannlpan@gmail.com>
2020-05-09 21:41:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,10 +31,10 @@ class Song
 
   def self.find_by_name(name)
     self.all.find do |song|
-      if song.name == name 
+      if song.name == name
          song
       end
-    end    
+    end
   end
 
   def self.find_or_create_by_name(name)
